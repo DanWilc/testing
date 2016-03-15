5e8bfe589bde5e93785eac9190824ebd10627d56
DanWilc <dullpepit0z@gmail.com>
2016-03-15 19:49:00 -0400

    Update jaba.java

--- src/jaba.java
+++ src/jaba.java
@@ -2,6 +2,6 @@
 public class jaba {
     public static String testing = "123";
     public static void main(String[] args){
-        System.out.println( testing);
+        System.out.println(testing);
     }
 }
